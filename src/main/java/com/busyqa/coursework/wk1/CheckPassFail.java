b69309ec75125f99023d249d61aa8893556207ed
ajaypaul007 <ajaypaul007@gmail.com>
2018-07-29 17:05:29 -0400

    modified CheckPassFail.java-working status

--- src/main/java/com/busyqa/coursework/wk1/CheckPassFail.java
+++ src/main/java/com/busyqa/coursework/wk1/CheckPassFail.java
@@ -2,14 +2,14 @@ package com.busyqa.coursework.wk1;
 
 public class CheckPassFail {  // Save as "CheckPassFail.java"
     public static void main(String[] args) {  // Program entry point
-        int mark = 49;  // Set the value of "mark" here!
+        int mark = 80;  // Set the value of "mark" here!
         System.out.println("The mark is " + mark);
 
-//        if ( ...... ) {
-//            System.out.println( ...... );
-//        } else {
-//            System.out.println( ...... );
-//        }
+        if ( mark > 60  ) {
+            System.out.println("You have passed!!" );
+        } else {
+           System.out.println("You have failed!!" );
+       }
         System.out.println( "done" );
     }
 }
