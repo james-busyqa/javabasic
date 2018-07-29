e2e146d57b0d6096852c287938b9f1c42e210311
ajaypaul007 <ajaypaul007@gmail.com>
2018-07-29 18:08:22 -0400

    updated int variable mark to 60

--- src/main/java/com/busyqa/coursework/wk1/CheckPassFail.java
+++ src/main/java/com/busyqa/coursework/wk1/CheckPassFail.java
@@ -2,13 +2,13 @@ package com.busyqa.coursework.wk1;
 
 public class CheckPassFail {  // Save as "CheckPassFail.java"
     public static void main(String[] args) {  // Program entry point
-        int mark = 80;  // Set the value of "mark" here!
+        int mark = 50;  // Set the value of "mark" here!
         System.out.println("The mark is " + mark);
 
-        if ( mark > 60  ) {
-            System.out.println("You have passed!!" );
+        if ( mark >= 50  ) {
+            System.out.println("PASS!!" );
         } else {
-           System.out.println("You have failed!!" );
+           System.out.println("FAIL!!" );
        }
         System.out.println( "done" );
     }
