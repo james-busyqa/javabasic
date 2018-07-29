caface0f341004f22b03ab001e2dc0427041bed1
ajaypaul007 <ajaypaul007@gmail.com>
2018-07-29 17:00:46 -0400

    modified CheckOddEven.java file - working

--- src/main/java/com/busyqa/coursework/wk1/CheckOddEven.java
+++ src/main/java/com/busyqa/coursework/wk1/CheckOddEven.java
@@ -4,11 +4,11 @@ public class CheckOddEven {
     public static void main(String[] args) {  // Program entry point
         int number = 50;       // Set the value of "number" here!
        System.out.println("The number is " + number);
-//        if ( ...... ) {
-//            System.out.println( ...... );
-//        } else {
-//            System.out.println( ...... );
-//        }
+       if (number%2 == 0 ) {
+           System.out.println(number + ": is even number");
+        } else {
+            System.out.println(number + ":is odd number");
+       }
 //        System.out.println( ...... );
     }
 }
