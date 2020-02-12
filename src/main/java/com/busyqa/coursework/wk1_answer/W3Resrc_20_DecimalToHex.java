719709a389f297a377db1960fd767a64128fc3fa
Meghana Rawate <meghana.rawate@gmail.com>
2020-02-11 19:43:44 -0500

    19,20,21 - Decimal to..

---
+++ src/main/java/com/busyqa/coursework/wk1_answer/W3Resrc_20_DecimalToHex.java
@@ -0,0 +1,17 @@
+package com.busyqa.coursework.wk1_answer;
+
+public class W3Resrc_20_DecimalToHex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Decimal number to convert to Binary
+		
+		int num = 101;
+		System.out.println(Integer.toHexString(num));
+		
+		
+
+	}
+
+}
---
+++ src/main/java/com/busyqa/coursework/wk1_answer/W3Resrc_21_DecimalToOctal.java
@@ -0,0 +1,15 @@
+package com.busyqa.coursework.wk1_answer;
+
+public class W3Resrc_21_DecimalToOctal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Decimal number to convert to Binary
+		
+		int num = 101;
+		System.out.println(Integer.toOctalString(num));
+
+	}
+
+}
